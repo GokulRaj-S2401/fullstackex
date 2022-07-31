7123b43ffb2310afd417089116f86ae5d30d192d
GokulRaj-S2401 <iamgokul2401@gmail.com>
2022-07-31 06:16:38 -0400

    create a form and validate using js

---
+++ 1/main.js
@@ -0,0 +1,45 @@
+document.getElementById('registerForm').addEventListener('submit',(e)=>{
+    let user = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let clgName = document.getElementById('clgName').value;
+    let mbleNo = document.getElementById('mobileNo').value;
+    let gender = document.getElementsByName('gender');
+    let genderValue = false
+    let error = [];
+
+    if( user.length <= 2 || !user.match((/^[a-z]+$/)) ){
+        error.push("invalid student name");
+    }
+
+    if(email.length < 5 || email.indexOf('@') == -1 || email.indexOf('.') == -1 ){
+        error.push("Invalid Email Address");
+    }
+
+    if(clgName.length < 5){
+        error.push("Invalid college name");
+    }
+
+    if(mbleNo.length != 10){
+        error.push("Invalid Phone Number");
+    }
+
+    // const g = gender.filter((element)=>element.checked)
+    for(let i=0;i<gender.length;i++){
+        if(gender[i].checked){
+           genderValue = true
+        }
+    }
+    
+    if(genderValue == false){
+        error.push("Plase select gender option")
+    }
+
+    if(error.length != 0){
+        e.preventDefault()
+        alert(error.map((data)=>`\n ${data}`))
+    }
+
+
+    
+
+});
